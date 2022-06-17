9b5bd1afffb163970f95d70cc48b0488f2d9728f
Roman <65543135+romanradice@users.noreply.github.com>
2022-06-17 15:37:44 +0200

    indentation

--- Befunge/main.cpp
+++ Befunge/main.cpp
@@ -10,7 +10,8 @@ int main()
 {
     string source = "code.txt";
     cout << "=========== Interpr�teur BEFUNGE ===========\n   Par : ROMANRAA    version : 1.0"<<endl;
-    try{
+    try
+	{
         if (source.size() < 1) throw runtime_error("Fichier d'entree non precise");
         ifstream read(source.c_str());
         if (read.fail()) throw runtime_error("Impossible de trouver le fichier precise");
